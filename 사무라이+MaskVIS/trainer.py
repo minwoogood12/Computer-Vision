b6ef0d14d843f9a5265163f354f4d6a13491986e
minwoogood12 <84243851+minwoogood12@users.noreply.github.com>
2025-07-19 20:19:51 +0900

    Update trainer.py

--- 사무라이+MaskVIS/trainer.py
+++ 사무라이+MaskVIS/trainer.py
@@ -546,32 +546,32 @@ class Trainer:
         outputs = model(batch)
         targets = batch.masks
         batch_size = len(batch.img_batch)
-        ##기존 maskfreevis는 이미지가 [B*T,C,H,W] 
-        ##SAM2는 image_batch : [B,T,C,H,W]
+        ##기존 maskfreevis는 이미지가 List[Tensor(3,H,W) * B * T] 
+        ##SAM2는 image_batch : [T,B,C,H,W] 즉 이거를 이미지텐서 * B, T로 바꿔야함
         ##       targets : [T, O, H, W]
         ##추가##
-        images = batch.img_batch.squeeze(1)  # shape: [T, B, C, H, W] T : 프레임수
-        
-        downsampled_images = F.avg_pool2d(images.float(), kernel_size=4, stride=4, padding=0)  # [T, C, H', W']
-        images_lab = [
-          torch.as_tensor(
-              color.rgb2lab(img[[2, 1, 0]].byte().permute(1, 2, 0).cpu().numpy()),
-              device=img.device,
-              dtype=torch.float32
-          ).permute(2, 0, 1)
-          for img in downsampled_images
-        ]
-        images_lab_sim = [get_images_color_similarity(img.unsqueeze(0), 3, 2) for img in images_lab]
-        images_lab_sim_nei = get_neighbor_images_patch_color_similarity(images_lab[0].unsqueeze(0), images_lab[1].unsqueeze(0), 3, 3)
-        images_lab_sim_nei1 = get_neighbor_images_patch_color_similarity(images_lab[1].unsqueeze(0), images_lab[2].unsqueeze(0), 3, 3)
-        images_lab_sim_nei2 = get_neighbor_images_patch_color_similarity(images_lab[2].unsqueeze(0), images_lab[3].unsqueeze(0), 3, 3)
-        images_lab_sim_nei3 = get_neighbor_images_patch_color_similarity(images_lab[3].unsqueeze(0), images_lab[4].unsqueeze(0), 3, 3)
-        images_lab_sim_nei4 = get_neighbor_images_patch_color_similarity(images_lab[4].unsqueeze(0), images_lab[5].unsqueeze(0), 3, 3)
-        images_lab_sim_nei5 = get_neighbor_images_patch_color_similarity(images_lab[5].unsqueeze(0), images_lab[6].unsqueeze(0), 3, 3)
-        images_lab_sim_nei6 = get_neighbor_images_patch_color_similarity(images_lab[6].unsqueeze(0), images_lab[7].unsqueeze(0), 3, 3)
-        images_lab_sim_nei7 = get_neighbor_images_patch_color_similarity(images_lab[7].unsqueeze(0), images_lab[0].unsqueeze(0), 3, 3)
-        ##추가##
-      
+        images = list(batch.img_batch.permute(1, 0, 2, 3, 4).reshape(-1, C, H, W))
+
+        k_size = 3 #3
+        rs_images = ImageList.from_tensors(images, self.size_divisibility)
+        #패딩처리해서 텐서로 묶음
+        downsampled_images = F.avg_pool2d(rs_images.tensor.float(), kernel_size=4, stride=4, padding=0) #for img in images]            #(3,H,W) -> (3,H//4, W//4)
+        images_lab = [torch.as_tensor(color.rgb2lab(ds_image[[2, 1, 0]].byte().permute(1, 2, 0).cpu().numpy()), device=ds_image.device, dtype=torch.float32).permute(2, 0, 1) for ds_image in downsampled_images]
+        #색 순서 변환 RGB->LAB변환
+        images_lab_sim = [get_images_color_similarity(img_lab.unsqueeze(0), k_size, 2) for img_lab in images_lab] # ori is 0.3, 0.5, 0.7
+        #List[Tensor(H,W)] * (B*T)
+        images_lab_sim_nei = [get_neighbor_images_patch_color_similarity(images_lab[ii].unsqueeze(0), images_lab[ii+1].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)] # ori dilation is 3
+        #List[Tensor(H,W)] * (B) 각각
+        images_lab_sim_nei1 = [get_neighbor_images_patch_color_similarity(images_lab[ii+1].unsqueeze(0), images_lab[ii+2].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
+        images_lab_sim_nei2 = [get_neighbor_images_patch_color_similarity(images_lab[ii+2].unsqueeze(0), images_lab[ii+3].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
+        images_lab_sim_nei3 = [get_neighbor_images_patch_color_similarity(images_lab[ii+3].unsqueeze(0), images_lab[ii+4].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
+        images_lab_sim_nei4 = [get_neighbor_images_patch_color_similarity(images_lab[ii+4].unsqueeze(0), images_lab[ii+5].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]##추가##
+        images_lab_sim_nei5 = [get_neighbor_images_patch_color_similarity(images_lab[ii+5].unsqueeze(0), images_lab[ii+6].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
+        images_lab_sim_nei6 = [get_neighbor_images_patch_color_similarity(images_lab[ii+6].unsqueeze(0), images_lab[ii+7].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
+        images_lab_sim_nei7 = [get_neighbor_images_patch_color_similarity(images_lab[ii+7].unsqueeze(0), images_lab[ii].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]##추가##
+
+
+##############################
         key = batch.dict_key  # key for dataset
 
         
