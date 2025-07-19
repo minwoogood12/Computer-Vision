2cf322ecb9f27f7c5a50ddf31b26e6f7e4bdbf4a
minwoogood12 <84243851+minwoogood12@users.noreply.github.com>
2025-07-20 03:15:08 +0900

    Update trainer.py

--- 사무라이+MaskVIS/trainer.py
+++ 사무라이+MaskVIS/trainer.py
@@ -569,15 +569,15 @@ class Trainer:
         #색 순서 변환 RGB->LAB변환
         images_lab_sim = [get_images_color_similarity(img_lab.unsqueeze(0), k_size, 2) for img_lab in images_lab] # ori is 0.3, 0.5, 0.7
         #List[Tensor(H,W)] * (B*T)
-        images_lab_sim_nei = [get_neighbor_images_patch_color_similarity(images_lab[ii].unsqueeze(0), images_lab[ii+1].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)] # ori dilation is 3
+        images_lab_sim_nei = [get_neighbor_images_patch_color_similarity(images_lab[ii].unsqueeze(0), images_lab[ii+1].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)] # ori dilation is 3
         #List[Tensor(H,W)] * (B) 각각
-        images_lab_sim_nei1 = [get_neighbor_images_patch_color_similarity(images_lab[ii+1].unsqueeze(0), images_lab[ii+2].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
-        images_lab_sim_nei2 = [get_neighbor_images_patch_color_similarity(images_lab[ii+2].unsqueeze(0), images_lab[ii+3].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
-        images_lab_sim_nei3 = [get_neighbor_images_patch_color_similarity(images_lab[ii+3].unsqueeze(0), images_lab[ii+4].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
-        images_lab_sim_nei4 = [get_neighbor_images_patch_color_similarity(images_lab[ii+4].unsqueeze(0), images_lab[ii+5].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]##추가##
-        images_lab_sim_nei5 = [get_neighbor_images_patch_color_similarity(images_lab[ii+5].unsqueeze(0), images_lab[ii+6].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
-        images_lab_sim_nei6 = [get_neighbor_images_patch_color_similarity(images_lab[ii+6].unsqueeze(0), images_lab[ii+7].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]
-        images_lab_sim_nei7 = [get_neighbor_images_patch_color_similarity(images_lab[ii+7].unsqueeze(0), images_lab[ii].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 5)]##추가##
+        images_lab_sim_nei1 = [get_neighbor_images_patch_color_similarity(images_lab[ii+1].unsqueeze(0), images_lab[ii+2].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)]
+        images_lab_sim_nei2 = [get_neighbor_images_patch_color_similarity(images_lab[ii+2].unsqueeze(0), images_lab[ii+3].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)]
+        images_lab_sim_nei3 = [get_neighbor_images_patch_color_similarity(images_lab[ii+3].unsqueeze(0), images_lab[ii+4].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)]
+        images_lab_sim_nei4 = [get_neighbor_images_patch_color_similarity(images_lab[ii+4].unsqueeze(0), images_lab[ii+5].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)]##추가##
+        images_lab_sim_nei5 = [get_neighbor_images_patch_color_similarity(images_lab[ii+5].unsqueeze(0), images_lab[ii+6].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)]
+        images_lab_sim_nei6 = [get_neighbor_images_patch_color_similarity(images_lab[ii+6].unsqueeze(0), images_lab[ii+7].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)]
+        images_lab_sim_nei7 = [get_neighbor_images_patch_color_similarity(images_lab[ii+7].unsqueeze(0), images_lab[ii].unsqueeze(0), 3, 3) for ii in range(0, len(images_lab), 8)]##추가##
 
 
 ##############################
