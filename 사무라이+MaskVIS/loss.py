963b289d5d259d6ed4fc499ff5599a8248377041
minwoogood12 <84243851+minwoogood12@users.noreply.github.com>
2025-07-20 01:20:48 +0900

    Update loss.py

--- 사무라이+MaskVIS/loss.py
+++ 사무라이+MaskVIS/loss.py
@@ -321,6 +321,7 @@ class MultiStepMultiMasksAndIous(nn.Module):
                 ##추가##
                 images_lab_sim, 
                 images_lab_sim_nei,
+                images_lab_sim_nei1,
                 images_lab_sim_nei2,
                 images_lab_sim_nei3,
                 images_lab_sim_nei4,
