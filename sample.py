374db89b5c195307b9cfe537639c60ce63f4f34b
minwoogood12 <84243851+minwoogood12@users.noreply.github.com>
2025-09-08 19:30:13 +0900

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,6 @@
+- _target_: training.dataset.transforms.ColorJitter
+          consistent_transform: False
+          brightness: 0.5
+          contrast: 0.5
+          saturation: 0.5
+          hue: null
